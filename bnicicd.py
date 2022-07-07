0372c57365091e25b4ae6422311be6b80f8388d1
dj-skr <diajengskr@gmail.com>
2022-07-07 14:29:39 +0700

    Add write file function

--- bnicicd.py
+++ bnicicd.py
@@ -8,7 +8,7 @@ response = requests.get("https://www.google.com")
 
 print(response.text)
 
-waktu = datetime.date.today()
+waktu = datetime.now()
 
 with open("tempResponse/" + str(waktu) + ".txt", "w") as f:
     f.write(response.text)
